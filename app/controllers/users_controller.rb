3813a31dc82c106588f96f388f4c84568db8e581
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-02-18 10:52:39 +0100

    add create user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(params_user)
+        if @user.save
+            flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up"
+            redirect_to articles_path
+        else
+            render 'new'
+        end        
+    end
+
+    private
+
+    def params_user
+        params.require(:user).permit(:username,:email,:password)
+    end
+end
